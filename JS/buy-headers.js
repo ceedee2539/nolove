f350f0077f415ad6f480300df17e9e82205bf299
ceedee2539 <kaotdvid@gmail.com>
2021-02-25 12:21:07 +0900

    1220: fix itunes buy method, add new check headers method

--- JS/buy-headers.js
+++ JS/buy-headers.js
@@ -4,9 +4,14 @@ $prefs.removeAllValues();
 
 if (headers["User-Agent"].indexOf("Presets") != -1) {
   $prefs.setValueForKey("true", "lrprst");
+} else {
+  $prefs.setValueForKey("false", "lrprst");
 }
+
 if (headers["User-Agent"].indexOf("Koloro") != -1) {
   $prefs.setValueForKey("true", "klr");
+} else {
+  $prefs.setValueForKey("false", "klr");
 }
 
 $done({headers});
