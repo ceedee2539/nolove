2cdd4a96cae1978b26ed4df31216823e45e6f4e1
ceedee2539 <kaotdvid@gmail.com>
2021-03-14 11:17:07 +0900

    autocad

--- JS/autocad.js
+++ JS/autocad.js
@@ -1,15 +1,11 @@
 var obj = JSON.parse($response.body);
 
-obj.userInfo.subscriptionsInfo=
+obj =
 {
-      "appId": 0,
-			"level": 10,
-			"expiration": 1975866787000,
-			"type": "ADSK_GRANTED_USERS_AUTODESK_EDUCATION_BUNDLE",
-			"subscribed": true,
-			"start_timestamp": 1581172387,
-			"expiry_timestamp": 1975866787
-    };
+  "level" : 10,
+  "expiry_milliseconds" : 4077569306000,
+  "expiry_timestamp" : 4077569306,
+  "start_timestamp" : 4077569306,
+  "grant_type" : "ADSK_GRANTED_USERS_AUTODESK_EDUCATION_BUNDLE"
+};
 $done({body: JSON.stringify(obj)});
-
-// Descriptions
