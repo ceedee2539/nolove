1627acfb31a6e736f8e467dd25f797838bbb6631
ceedee2539 <kaotdvid@gmail.com>
2021-03-14 09:47:42 +0900

    cmr360

--- JS/camera360.vip.js
+++ JS/camera360.vip.js
@@ -5,8 +5,8 @@ obj = {
         "sandbox":0,
         "isTrialPeriod":0,
         "purchaseTime":1598155239,
-        "expireTime":4122936039,
-        "appleExpireTime":4122936039,
+        "expireTime":4091140839,
+        "appleExpireTime":4091140839,
         "originalTransactionId":"540000568481558",
         "productId":"VIP_yearly_29.99",
         "appleVip":1,
@@ -16,7 +16,7 @@ obj = {
     "status":200,
     "message":"ok",
     "serverTime":1598155311.9284,
-    "exetime":"1598155309028-1598155311928"
+    "exetime":"4091140909000-4091140912000"
 };
 $done({body: JSON.stringify(obj)});
 
